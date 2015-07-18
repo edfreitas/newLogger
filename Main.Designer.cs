9b51da507f7f4560231437ecf2d42c9a94a36ef0
Ed Freitas <fastapps@outlook.com>
2015-07-18 12:08:52 +0200

    newLogger
    Init

---
+++ Main.Designer.cs
@@ -0,0 +1,252 @@
+﻿/*
+    Copyright (c) 2015 <a href="http://edfreitas.me">Ed Freitas</a>
+ 
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+ */
+
+namespace newLogger
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.mtRecords = new MetroFramework.Controls.MetroLabel();
+            this.mtClean = new MetroFramework.Controls.MetroTile();
+            this.mtSave = new MetroFramework.Controls.MetroTile();
+            this.mtStop = new MetroFramework.Controls.MetroTile();
+            this.mtStart = new MetroFramework.Controls.MetroTile();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.dataGridSearch = new System.Windows.Forms.DataGridView();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.saveWorker = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSearch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer.IsSplitterFixed = true;
+            this.splitContainer.Location = new System.Drawing.Point(20, 60);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.mtRecords);
+            this.splitContainer.Panel1.Controls.Add(this.mtClean);
+            this.splitContainer.Panel1.Controls.Add(this.mtSave);
+            this.splitContainer.Panel1.Controls.Add(this.mtStop);
+            this.splitContainer.Panel1.Controls.Add(this.mtStart);
+            this.splitContainer.Panel1.Controls.Add(this.tbSearch);
+            this.splitContainer.Panel1MinSize = 45;
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.dataGridSearch);
+            this.splitContainer.Panel2.Controls.Add(this.dataGridView);
+            this.splitContainer.Size = new System.Drawing.Size(812, 342);
+            this.splitContainer.SplitterDistance = 60;
+            this.splitContainer.TabIndex = 9;
+            // 
+            // mtRecords
+            // 
+            this.mtRecords.AutoSize = true;
+            this.mtRecords.Location = new System.Drawing.Point(395, 32);
+            this.mtRecords.Name = "mtRecords";
+            this.mtRecords.Size = new System.Drawing.Size(17, 20);
+            this.mtRecords.TabIndex = 14;
+            this.mtRecords.Text = "0";
+            // 
+            // mtClean
+            // 
+            this.mtClean.Location = new System.Drawing.Point(93, 28);
+            this.mtClean.Name = "mtClean";
+            this.mtClean.Size = new System.Drawing.Size(109, 28);
+            this.mtClean.TabIndex = 13;
+            this.mtClean.Text = "Clear View";
+            this.mtClean.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mtClean.Click += new System.EventHandler(this.mtClean_Click);
+            // 
+            // mtSave
+            // 
+            this.mtSave.Location = new System.Drawing.Point(3, 28);
+            this.mtSave.Name = "mtSave";
+            this.mtSave.Size = new System.Drawing.Size(84, 28);
+            this.mtSave.TabIndex = 12;
+            this.mtSave.Text = "Save";
+            this.mtSave.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mtSave.Click += new System.EventHandler(this.mtSave_Click_1);
+            // 
+            // mtStop
+            // 
+            this.mtStop.Location = new System.Drawing.Point(298, 28);
+            this.mtStop.Name = "mtStop";
+            this.mtStop.Size = new System.Drawing.Size(84, 28);
+            this.mtStop.TabIndex = 11;
+            this.mtStop.Text = "Stop";
+            this.mtStop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mtStop.Click += new System.EventHandler(this.mtStop_Click_1);
+            // 
+            // mtStart
+            // 
+            this.mtStart.Location = new System.Drawing.Point(208, 28);
+            this.mtStart.Name = "mtStart";
+            this.mtStart.Size = new System.Drawing.Size(84, 28);
+            this.mtStart.TabIndex = 10;
+            this.mtStart.Text = "Start";
+            this.mtStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mtStart.Click += new System.EventHandler(this.mtStart_Click_1);
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tbSearch.Location = new System.Drawing.Point(0, 0);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(812, 22);
+            this.tbSearch.TabIndex = 9;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
+            // 
+            // dataGridSearch
+            // 
+            this.dataGridSearch.AllowUserToAddRows = false;
+            this.dataGridSearch.AllowUserToDeleteRows = false;
+            this.dataGridSearch.AllowUserToOrderColumns = true;
+            this.dataGridSearch.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridSearch.BackgroundColor = System.Drawing.Color.AliceBlue;
+            this.dataGridSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridSearch.GridColor = System.Drawing.Color.LightSkyBlue;
+            this.dataGridSearch.Location = new System.Drawing.Point(0, 0);
+            this.dataGridSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridSearch.Name = "dataGridSearch";
+            this.dataGridSearch.ReadOnly = true;
+            this.dataGridSearch.Size = new System.Drawing.Size(812, 278);
+            this.dataGridSearch.TabIndex = 6;
+            this.dataGridSearch.VirtualMode = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.AliceBlue;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.GridColor = System.Drawing.Color.LightSkyBlue;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(812, 278);
+            this.dataGridView.TabIndex = 5;
+            this.dataGridView.VirtualMode = true;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // saveWorker
+            // 
+            this.saveWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.saveWorker_DoWork);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 422);
+            this.Controls.Add(this.splitContainer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(526, 362);
+            this.Name = "Main";
+            this.Text = "Event Inspector";
+            this.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.Load += new System.EventHandler(this.OnLoadForm);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSearch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private MetroFramework.Controls.MetroTile mtSave;
+        private MetroFramework.Controls.MetroTile mtStop;
+        private MetroFramework.Controls.MetroTile mtStart;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Timer timer;
+        private MetroFramework.Controls.MetroTile mtClean;
+        private MetroFramework.Controls.MetroLabel mtRecords;
+        private System.ComponentModel.BackgroundWorker saveWorker;
+        private System.Windows.Forms.DataGridView dataGridSearch;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,396 @@
+﻿/*
+    Copyright (c) 2015 <a href="http://edfreitas.me">Ed Freitas</a>
+ 
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Timers;
+using System.Diagnostics.Eventing.Reader;
+using System.Threading;
+using System.Configuration;
+
+using MetroFramework;
+using System.IO;
+
+namespace newLogger
+{
+    public partial class Main : MetroFramework.Forms.MetroForm
+    {
+        private int lastLogCount = 0;
+        private bool changed = false;
+
+        private string saveFileName = String.Empty;
+        private List<string> settings = new List<string>();
+
+        public static DataTable dtLogger = new DataTable();      
+        public static DataGridView dgv = new DataGridView();
+
+        public static DataTable dtSearch = new DataTable();
+        public static DataGridView dgvSearch = new DataGridView();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void OrganiseGrid()
+        {
+            try
+            {
+                dataGridSearch.Visible = false;
+
+                dgvSearch = dataGridSearch;
+                dgv = dataGridView;
+
+                dtLogger.Columns.Add("Message", typeof(string));
+                dtLogger.Columns.Add("Logged", typeof(string));
+                dtLogger.Columns.Add("Level", typeof(string));
+                dtLogger.Columns.Add("User", typeof(string));
+
+                dtSearch.Columns.Add("Message", typeof(string));
+                dtSearch.Columns.Add("Logged", typeof(string));
+                dtSearch.Columns.Add("Level", typeof(string));
+                dtSearch.Columns.Add("User", typeof(string));
+
+                dtLogger.Rows.Clear();
+                dtSearch.Rows.Clear();
+
+                dgv.Refresh();
+            }
+            catch { }
+        }
+
+        private void LoadSettings()
+        {
+            try
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                if (config != null)
+                {
+                    foreach (string key in ConfigurationManager.AppSettings)
+                    {
+                        settings.Add(ConfigurationManager.AppSettings[key]);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void AddRow(EventLogEntry entry)
+        {
+            try
+            {
+                object[] dr = new object[] { entry.Message, entry.TimeGenerated.ToString(), entry.EntryType.ToString(),
+                (entry.UserName != null) ? entry.UserName : "n/a" };
+
+                dtLogger.Rows.Add(dr);
+            }
+            catch { }
+        }
+
+        private void ReadLogger()
+        {
+            try
+            {
+                if (settings != null && settings.Count > 0)
+                {
+                    using (EventLog demoLog = new EventLog(settings.ToArray()[0]))
+                    {
+                        if (demoLog != null)
+                        {
+                            EventLogEntryCollection entries = demoLog.Entries;
+
+                            if (entries != null && entries.Count > 0)
+                            {
+                                if (entries.Count > 0 && entries.Count != lastLogCount)
+                                {
+                                    if (lastLogCount == 0)
+                                    {
+                                        foreach (EventLogEntry entry in entries)
+                                        {
+                                            AddRow(entry);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        EventLogEntry entry = entries[entries.Count - 1];
+                                        AddRow(entry);
+                                    }
+
+                                    changed = true;
+                                    lastLogCount = entries.Count;
+                                }
+                                else
+                                    changed = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void OnLoadForm(object sender, EventArgs e)
+        {
+            try
+            {
+                disableStartStop(false, true);
+                LoadSettings();
+                OrganiseGrid();
+
+                RunEntries();
+            }
+            catch { }
+        }
+
+        private void RunEntries()
+        {
+            if (!backgroundWorker.IsBusy && !saveWorker.IsBusy)
+            {
+                backgroundWorker.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ReadLogger();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (changed)
+            {
+                mtRecords.Text = lastLogCount.ToString();
+
+                dgv.DataSource = dtLogger;
+                dgv.Columns[0].Width = this.Width;
+
+                if (dgv.Rows.Count > 0)
+                {
+                    dgv.Rows[0].Cells[0].Selected = false;
+                    dgv.Rows[dgv.Rows.Count - 1].Cells[0].Selected = true;
+                    dgv.FirstDisplayedScrollingRowIndex = dgv.Rows.Count - 1;
+
+                    dgv.Refresh();
+                }
+            }
+
+            RunEntries();
+        }
+
+        private void mtClean_Click(object sender, EventArgs e)
+        {
+            dtLogger.Rows.Clear();
+            lastLogCount = 0;
+            mtRecords.Text = lastLogCount.ToString();
+        }
+
+        private void disableStartStop(bool start, bool stop)
+        {
+            mtStart.Enabled = start;
+            mtStop.Enabled = stop;
+        }
+
+        private void mtStart_Click_1(object sender, EventArgs e)
+        {
+            timer.Start();
+
+            disableStartStop(false, true);
+        }
+
+        private void mtStop_Click_1(object sender, EventArgs e)
+        {
+            timer.Stop();
+
+            disableStartStop(true, false);
+        }
+
+        private void SaveFile()
+        {
+            try
+            {
+                DataGridView dg = (dataGridSearch.Visible) ? dgvSearch : dgv;
+                DataTable dt = (dataGridSearch.Visible) ? dtSearch : dtLogger;
+
+                using (StreamWriter str = new StreamWriter(Path.Combine(Path.GetDirectoryName(saveFileName), 
+                    Path.GetFileNameWithoutExtension(saveFileName)) + "_" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm") + ".txt"))
+                {
+                    for (int i = 0; i <= dg.Rows.Count - 1; i++)
+                    {
+                        if (dg.Rows[i].Cells[0].Selected == true)
+                        {
+                            str.WriteLine(">>>> LogEntry Start ---> " + str.NewLine +
+                                str.NewLine + "DateTime: " + dt.Rows[i][1] + 
+                                str.NewLine + "Type: " + dt.Rows[i][2] + 
+                                str.NewLine + "User: " + dt.Rows[i][3] + 
+                                str.NewLine + "Message: " + dt.Rows[i][0] + 
+                                str.NewLine + str.NewLine + "<--- LogEntry End <<<<" + 
+                                str.NewLine + str.NewLine);
+                        }
+                    }
+
+                    str.Close();
+                }
+            }
+            catch { }
+        }
+
+        private void saveWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void mtSave_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                int count = (dataGridSearch.Visible) ? dgvSearch.RowCount : dgv.RowCount;
+
+                if (dgv != null && dgvSearch != null && count > 0)
+                {
+                    using (SaveFileDialog dialog = new SaveFileDialog())
+                    {
+                        dialog.Filter = "txt files (*.txt)|*.txt";
+                        dialog.RestoreDirectory = true;
+
+                        if (dialog.ShowDialog() == DialogResult.OK)
+                        {
+                            saveFileName = dialog.FileName;
+
+                            if (!saveWorker.IsBusy)
+                                saveWorker.RunWorkerAsync();
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void SearchGrid(string txt)
+        {
+            try
+            {
+                dtSearch.Rows.Clear();
+
+                for (int i = 0; i <= dgv.Rows.Count - 1; i++)
+                {
+                    if (dtLogger.Rows[i][0].ToString().ToUpper().Contains(txt.ToUpper()) ||
+                        dtLogger.Rows[i][1].ToString().ToUpper().Contains(txt.ToUpper()) ||
+                        dtLogger.Rows[i][2].ToString().ToUpper().Contains(txt.ToUpper()) ||
+                        dtLogger.Rows[i][3].ToString().ToUpper().Contains(txt.ToUpper()))
+                    {
+                        object[] dr = new object[] { dtLogger.Rows[i][0].ToString(),
+                            dtLogger.Rows[i][1].ToString(), dtLogger.Rows[i][2].ToString(),
+                            dtLogger.Rows[i][3].ToString() };
+
+                        
+                        dtSearch.Rows.Add(dr);
+                    }
+                }
+
+                dgvSearch.DataSource = dtSearch;
+                dgvSearch.Columns[0].Width = this.Width;
+
+                if (dgvSearch.Rows.Count > 0)
+                {
+                    dataGridSearch.Visible = true;
+                    mtRecords.Text = dgvSearch.Rows.Count.ToString();
+
+                    dgvSearch.Rows[0].Cells[0].Selected = false;
+                    dgvSearch.Rows[dgvSearch.Rows.Count - 1].Cells[0].Selected = true;
+
+                    dgvSearch.FirstDisplayedScrollingRowIndex = dgvSearch.Rows.Count - 1;
+                    dgvSearch.Refresh();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void GetStartStopState()
+        {
+            mtStart.Visible = false;
+            mtStop.Visible = false;
+        }
+
+        private void SetStartStopState()
+        {
+            mtStart.Visible = true;
+            mtStop.Visible = true;
+        }
+
+        private void DoSearch()
+        {
+            if (tbSearch.Text != String.Empty)
+            {
+                mtClean.Enabled = false;
+                GetStartStopState();
+
+                SearchGrid(tbSearch.Text);
+            }
+            else
+            {
+                dataGridSearch.Visible = false;
+                mtRecords.Text = dgv.Rows.Count.ToString();
+
+                SetStartStopState();
+                mtClean.Enabled = true;
+            }
+        }
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            DoSearch();
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            if (dataGridView.Visible)
+            {
+                if (dgv.RowCount > 0)
+                {
+                    dgv.FirstDisplayedScrollingRowIndex = dgv.Rows.Count - 1;
+                }
+            }
+            else
+            {
+                if (dgvSearch.RowCount > 0)
+                {
+                    dgvSearch.FirstDisplayedScrollingRowIndex = dgvSearch.Rows.Count - 1;
+                }
+            }
+        }
+    }
+}
